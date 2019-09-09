065f0a2e649abbee658ef78d672ac16b32f16474
nparfen <nktparfen@gmail.com>
2019-09-09 14:02:41 +0300

    Added react-todo-app

---
+++ react-todo-app/precache-manifest.0b816c3df994f4b6607bd9a2c2d48389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae4e831975e2c4d2d2141b3c84b4935",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "262ef4edb010f29fc1d9",
+    "url": "/react-todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a808a14d51d3913b35ce",
+    "url": "/react-todo-app/static/js/2.1252ff9d.chunk.js"
+  },
+  {
+    "revision": "262ef4edb010f29fc1d9",
+    "url": "/react-todo-app/static/js/main.45c31197.chunk.js"
+  },
+  {
+    "revision": "3c59aee5d88ed462e03b",
+    "url": "/react-todo-app/static/js/runtime~main.136954d8.js"
+  }
+]);
