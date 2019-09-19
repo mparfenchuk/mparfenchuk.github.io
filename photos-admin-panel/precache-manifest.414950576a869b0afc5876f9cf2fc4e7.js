2b13df15ace8fc1116c1e5337f85669f0b8cf5cf
nparfen <nktparfen@gmail.com>
2019-09-19 17:41:37 +0300

    Added photos-admin-panel

---
+++ photos-admin-panel/precache-manifest.414950576a869b0afc5876f9cf2fc4e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d300adbf219834cad056a2e8bc2f7462",
+    "url": "/photos-admin-panel/index.html"
+  },
+  {
+    "revision": "2dbb9d36b1b2dc67eeb0",
+    "url": "/photos-admin-panel/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "2dbb9d36b1b2dc67eeb0",
+    "url": "/photos-admin-panel/static/js/2.be5cf514.chunk.js"
+  },
+  {
+    "revision": "4a51a5a71afdaa8d2179",
+    "url": "/photos-admin-panel/static/js/main.dd97977f.chunk.js"
+  },
+  {
+    "revision": "1ec90bbb4d164d19cd9e",
+    "url": "/photos-admin-panel/static/js/runtime~main.be5af0c9.js"
+  }
+]);
