01d82021fa11c69b7e9e7fae2409bb9025d88b41
nparfen <nktparfen@gmail.com>
2019-10-23 20:03:07 +0300

    Push new project

---
+++ diary-app/precache-manifest.f8d896a614b3385d5da5bd659d8dea9b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c017d95f8f9eec6f05232c1e203640",
+    "url": "/diary-app/index.html"
+  },
+  {
+    "revision": "252c8fae05a91ba90321",
+    "url": "/diary-app/static/js/2.7d16b26c.chunk.js"
+  },
+  {
+    "revision": "7b13df454754b21d7e88",
+    "url": "/diary-app/static/js/main.cdffcd8e.chunk.js"
+  },
+  {
+    "revision": "276cabf1022acee7c7bf",
+    "url": "/diary-app/static/js/runtime-main.8d9f8755.js"
+  }
+]);
