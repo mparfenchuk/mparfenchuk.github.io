51cf06f6a99d62909aae4ade4ab9401ba83dc239
nparfen <nktparfen@gmail.com>
2020-01-24 10:21:47 +0200

    Added new project

---
+++ logo-uploader/precache-manifest.9fa587a307e8a8425399c078ea779ccb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6f9fca4f56590b87ec045b3b9fa832",
+    "url": "/logo-uploader/index.html"
+  },
+  {
+    "revision": "7bb0190c7f7fcbd61358",
+    "url": "/logo-uploader/static/css/main.4dbb23c3.chunk.css"
+  },
+  {
+    "revision": "602239837f2bcbebb1d1",
+    "url": "/logo-uploader/static/js/2.351f79e3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/logo-uploader/static/js/2.351f79e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bb0190c7f7fcbd61358",
+    "url": "/logo-uploader/static/js/main.9448d233.chunk.js"
+  },
+  {
+    "revision": "2fcf32de9e9032bbc8a6",
+    "url": "/logo-uploader/static/js/runtime-main.8fd84030.js"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/logo-uploader/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/logo-uploader/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/logo-uploader/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/logo-uploader/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/logo-uploader/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/logo-uploader/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "2db17e0cdcc910857d22cd1d8994456b",
+    "url": "/logo-uploader/static/media/building.2db17e0c.svg"
+  }
+]);
